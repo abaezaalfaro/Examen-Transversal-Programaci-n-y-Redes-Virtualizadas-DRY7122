926a4fd2f840c45ecb600e4db9d6eb9d0c4306c1
abaezaalfaro <arie.baeza@duocuc.cl>
2025-07-06 18:40:43 -0400

    integrantes.py

---
+++ integrantes.py
@@ -0,0 +1,10 @@
+integrantes = [
+    "Cristian Pinto",
+    "Carlos Evert",
+    "Ariel Baeza" ,
+    "Jose Rodriguez"
+]
+
+print("Integrantes del grupo:")
+for integrante in integrantes:
+    print(f"- {integrante}")
